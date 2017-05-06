8a97e7e22f078c3231215fa5df5fb0487a7829c9
Mihail Tsurkanov <mtsurkanov@grossum.com>
2017-05-06 17:13:35 +0300

    add project controller, reorganized templates

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,19 +2,22 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @author Tsurkanov Mihail <tsurkanovm@gmail.com>
+ */
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('@App/Home/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('home/index.html.twig');
     }
 }
